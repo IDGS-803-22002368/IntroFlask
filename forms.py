736946384652a7909633dc8bab3f840a27aac100
Alexis Alvarez <alexis1234563@outlook.com>
2025-02-24 20:57:47 -0600

    final

--- forms.py
+++ forms.py
@@ -2,11 +2,22 @@ from wtforms import Form
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange
 from wtforms import StringField, EmailField, PasswordField, SubmitField, IntegerField, SelectField, DecimalField
+from wtforms import validators
 
 
 class UserForm(Form):
-    matricula = StringField("Matricula")
-    edad = IntegerField("Edad")
-    nombre = StringField("Nombre")
-    apellidos = StringField("Apellidos")
-    email = EmailField("Correo")
+    matricula = StringField("Matricula", [
+        validators.DataRequired("Este campo es requerido")
+    ])
+    edad = IntegerField("Edad", [
+        validators.DataRequired("Este campo es requerido")
+    ])
+    nombre = StringField("Nombre", [
+        validators.DataRequired("Este campo es requerido")
+    ])
+    apellidos = StringField("Apellidos", [
+        validators.DataRequired("Este campo es requerido")
+    ])
+    email = EmailField("Correo", [
+        validators.Email(message="Ingrese un correo valido")
+    ])
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template, request, url_for
+import forms
+from flask import g
+from flask_wtf.csrf import CSRFProtect
+from flask import flash
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.secret_key = "Esta es la clave secreta"
+csrf = CSRFProtect()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.before_request
+def before_request():
+    g.nombre = "Mario"
+    print("Antes de la peticion 1")
+
+
+@app.after_request
+def after_request(response):
+    print("Despues de la peticion 3")
+    return response
 
 
 @app.route("/")
 def index():
     grupo = "IDGS803"
-    listaAlumnos = ["Alexis", "Erick", "Jorge", "Luis", "Miguel", "Oscar", "Ricardo", "Sergio", "Victor"]
+    listaAlumnos = ["Alexis", "Erick", "Jorge", "Luis",
+                    "Miguel", "Oscar", "Ricardo", "Sergio", "Victor"]
+    print("Index 2")
+    print("Hola", g.nombre)
     return render_template("index.html", grupo=grupo, listaAlumnos=listaAlumnos)
 
 
@@ -24,23 +50,27 @@ def resultado():
         num2 = request.form.get('num2')
         tipo = request.form.get('tipo')
         if num2 == "" or num1 == "":
-            mensaje = "No puedes tener campos vacios"    
+            mensaje = "No puedes tener campos vacios"
         elif tipo == 'suma':
             resultado = int(num1) + int(num2)
-            mensaje = mensaje + "la suma de " + num1 + " y " + num2 + " es: " + str(resultado)
+            mensaje = mensaje + "la suma de " + num1 + \
+                " y " + num2 + " es: " + str(resultado)
         elif tipo == 'resta':
             resultado = int(num1) - int(num2)
-            mensaje = mensaje + "la resta de " + num1 + " y " + num2 + " es: " + str(resultado)
+            mensaje = mensaje + "la resta de " + num1 + \
+                " y " + num2 + " es: " + str(resultado)
         elif tipo == 'multiplicacion':
             resultado = int(num1) * int(num2)
-            mensaje = mensaje + "la multiplicacion de " + num1 + " y " + num2 + " es: " + str(resultado)
+            mensaje = mensaje + "la multiplicacion de " + \
+                num1 + " y " + num2 + " es: " + str(resultado)
         elif tipo == 'division':
             if num2 == '0':
                 resultado = "No se puede dividir entre 0"
                 mensaje = f"No se puede dividir entre 0"
             else:
                 resultado = int(num1) / int(num2)
-                mensaje = mensaje + "la division de " + num1 + " y " + num2 + " es: " + str(resultado)
+                mensaje = mensaje + "la division de " + num1 + \
+                    " y " + num2 + " es: " + str(resultado)
         else:
             resultado = "No se selecciono ninguna operacion"
         return render_template("OperasBas.html", resultado=resultado, num1=num1, num2=num2, mensaje=mensaje)
@@ -54,46 +84,6 @@ def ejemplo1():
 @app.route("/ejemplo2")
 def ejemplo2():
     return render_template("ejemplo2.html")
-
-# @app.route('/hola')
-# def hola():
-#     return 'Hola!!!'
-
-# @app.route("/user/<string:user>")
-# def user(user):
-#     return f"Hola {user}!!!"
-
-# @app.route("/numero/<int:num>")
-# def numero(num):
-#     return f"Numero: {num}"
-
-# @app.route("/user/<string:user>/<int:ID>")
-# def username(user, ID):
-#     return f"Hola {user}!!! Tu id es: {ID}"
-
-# @app.route("/suma/<float:num1>/<float:num2>")
-# def suma(num1, num2):
-#     return f"La suma es: {num1 + num2}"
-
-# @app.route("/default")
-# @app.route("/default/<string:nombre>")
-# def func(nombre='Alexis'):
-#     return "El nombre de nom es " + nombre
-
-# @app.route("/form1", methods=['GET', 'POST'])
-# def form1():
-#     return '''
-#     <form action="/form1" method="POST">
-#         <input type="text" name="nombre">
-#         <input type="submit" value="Enviar" onclick="enviar()">
-#     </form>
-#     <script>
-#         nombre = document.querySelector("input[name=nombre]");
-#         function enviar() {
-#             alert("Hola" + " " + nombre.value);
-#         }
-#     </script>
-# '''
 
 
 @app.route("/cine", methods=['GET', 'POST'])
@@ -129,12 +119,32 @@ def procesarBoleto():
                 descuento_total = descuento_total + 0.10
             else:
                 descuento_total = descuento_total
-            
+
             subtotal = cantidad_boletos * precio
             total = subtotal - (subtotal * descuento_total)
             mensaje = f"{nombre} tu compra ha sido exitosa, el total a pagar es: {total} y tu total de boletos es {cantidad_boletos}"
         return render_template("cine.html", nombre=nombre, cantidad_compradores=cantidad_compradores, tarjeta=tarjeta, cantidad_boletos=cantidad_boletos, mensaje=mensaje, descuento_total=descuento_total, subtotal=subtotal, total=total)
 
 
+@app.route("/alumnos", methods=["GET", "POST"])
+def alumnos():
+    mat = ''
+    nom = ''
+    edad = ''
+    correo = ''
+    ape = ''
+    alumno_clase = forms.UserForm(request.form)
+    if request.method == 'POST' and alumno_clase.validate():
+        mat = alumno_clase.matricula.data
+        nom = alumno_clase.nombre.data
+        edad = alumno_clase.edad.data
+        correo = alumno_clase.email.data
+        ape = alumno_clase.apellidos.data
+        mensaje = "Bienvenido {}".format(nom)
+        flash(mensaje)
+    return render_template("alumnos.html", form=alumno_clase, mat=mat, nom=nom, edad=edad, correo=correo, ape=ape)
+
+
 if __name__ == '__main__':
+    csrf.init_app(app)
     app.run(debug=True, port=3000)
